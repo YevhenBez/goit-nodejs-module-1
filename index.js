344f3de851b4848b3b1e0cf01c022d6e56fe4a60
YevhenBez <byxgalter@bigmir.net>
2023-07-04 20:43:55 +0300

    Просто причёсывание под условия ТЗ. Модуль 1   Node.js
    Просто причёсывание под условия ТЗ. Модуль 1   Node.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const contacts = require("./contacts")
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
-        case "read":
+        case "list":
             const allContacts = await contacts.listContacts();
-            return console.log(allContacts);
+        return console.table(allContacts);
     }
 }
 
