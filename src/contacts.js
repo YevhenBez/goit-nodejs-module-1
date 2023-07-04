8a68038a9f5c3808eabc66f92eb69762fa6b21d8
YevhenBez <byxgalter@bigmir.net>
2023-07-04 11:26:44 +0300

    Написал функцию получения всех контактов и экспортнул её.  Модуль 1   Node.js
    Написал функцию получения всех контактов   listContacts   в которой  при помощи метода   readFile   модуля    fs  получил все контакты   в    data   и затем распарсил их. Затем экспортнул данную функцию    listContacts   при помощи     module.exports
     .  Модуль 1   Node.js

--- src/contacts.js
+++ src/contacts.js
@@ -3,3 +3,12 @@ const fs = require("fs/promises");
 const path = require("path");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+}
+
+module.exports = {
+    listContacts
+}
